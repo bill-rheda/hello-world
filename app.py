bdd43af3eed53fdd4217eae4936641f24fdc858b
bill-rheda <45284090+bill-rheda@users.noreply.github.com>
2018-11-28 15:20:25 +0100

    Add files via upload
    This is a client script that checks the connection between a client and the server and if the connection exist, it sends a message to the server. Hopes this helps!

---
+++ app.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+output1 = "OK, CONNECTION SUCCEEDED"
+output2 ="NOT OK, CONNECTION FAILED"
+
+def check_tcp_connection(IP):
+    port = 8443
+    try:
+        client.connect(('IP', port))
+        return output1 
+    except socket.error:
+        return output2 
+check_tcp_connection('42.206.4.219')
+
+
+def test_case(IP):
+    if (check_tcp_connection(IP) == output1):
+        print ("Checking the connectivity......................................OK! CONNECTION SUCCEEDED")
+    else:
+        print("Checking the connectivity......................................NOT OK! CONNECTION FAILED")
+test_case('42.206.4.219')
+
+
+def send_sms(IP):
+    message ="Hey! How are you doing? From FRIEDASMS Ltd"
+    try:
+        client.sendall(message)
+    except:
+        print ("ERROR IN SENDING MESSAGE")
+send_sms('42.206.4.219')
+
+
+
+
+    
+
+   
+
+
+
+
+    
+
