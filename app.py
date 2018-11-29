0441d13f292dba1ee45d3153d7b10d31a1cfc8dc
bill-rheda <45284090+bill-rheda@users.noreply.github.com>
2018-11-29 10:00:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
+#This is a client script meant to check the TCP connection with its server and return a result
 import socket
 import sys
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 output1 = "OK, CONNECTION SUCCEEDED"
 output2 ="NOT OK, CONNECTION FAILED"
 
-def check_tcp_connection(IP):
+def check_tcp_connection(IP):#This defines the function that connects the client to the server.The function has as parameter the IP of the server
     port = 8443
     try:
         client.connect(('IP', port))
         return output1 
     except socket.error:
         return output2 
-check_tcp_connection('0.0.0.0')
+check_tcp_connection('0.0.0.0')#This function only test the connection and returns either output one or two but does not print out the result
 
 
-def test_case(IP):
-    if (check_tcp_connection(IP) == output1):
+def test_case(IP):#This is a test for the function above.It has as parameter the IP of server and prints out the result from the test
+    if (check_tcp_connection(IP) == output1):#if your client is connected to your server, then this would be printed on your screen
         print ("Checking the connectivity......................................OK! CONNECTION SUCCEEDED")
-    else:
+    else:#if not, it indicates that your I is not connected to the server and would have this printed on your screen
         print("Checking the connectivity......................................NOT OK! CONNECTION FAILED")
 test_case('0.0.0.0')
 
 
-def send_sms(IP):
+def send_sms(IP):#This function sends mesages to the IP of the server given that the connection has been established
     message ="Hey! How are you doing? From FRIEDASMS Ltd"
     try:
-        client.sendall(message)
+        client.sendall(message)#If a connection is established, then the message above would be sent to your server
     except:
-        print ("ERROR IN SENDING MESSAGE")
+        print ("ERROR IN SENDING MESSAGE")#If no connection exist, then the mesage would not be sent and "ERROR IN SENDING MESSAGE" would be displayed on your screen
 send_sms('0.0.0.0')
 
 
